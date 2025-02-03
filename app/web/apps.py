125223609537db9159b4e1a02a95f672b4325dd1
Jaromir007 <jaromir.obitko@student.gyarab.cz>
2025-02-03 08:41:16 +0100

    rename app name: slicer -> web

--- app/web/apps.py
+++ app/web/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SlicerConfig(AppConfig):
+class WebConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'slicer'
+    name = 'web'
