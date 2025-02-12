2ba45f02c29c240a79d10b3c2d016f2d4c0a5e70
Jaromir007 <jaromir.obitko@student.gyarab.cz>
2025-02-12 12:59:49 +0100

    mixel login template

--- app/web/views.py
+++ app/web/views.py
@@ -9,3 +9,7 @@ from django.template import loader
 def home(request):
     template = loader.get_template('views/home.html')
     return HttpResponse(template.render())
+
+def login(request): 
+    template = loader.get_template('views/login.html')
+    return HttpResponse(template.render())
