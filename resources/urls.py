aba4afe85de3973b4bcca0963f545ef661ea5517
Michal Drápal <michal.drapal@student.gyarab.cz>
2025-03-29 01:44:01 +0100

    add blog

--- resources/urls.py
+++ resources/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('upload', views.upload, name='upload'),
+    path('blog', views.postlist, name='blog'),
+    path('blog/<slug:post_slug>/', views.post, name='post')
 ]
